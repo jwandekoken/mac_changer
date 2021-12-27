b17bd296b130d6b3d660c2642614daa74f54b342
Julio Wandekoken <jwandekoken@gmail.com>
2021-12-27 12:21:43 -0500

    added checking for the required args

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='Changes the informed network interface MAC address.')
-parser.add_argument('-i', '--interface', dest='interface', help='Interface to change its MAC address')
-parser.add_argument('-m', '--mac', dest='new_mac', help='New MAC address')
 
-args = parser.parse_args()
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Changes the informed network interface MAC address.')
+    parser.add_argument('-i', '--interface', dest='interface', help='Interface to change its MAC address')
+    parser.add_argument('-m', '--mac', dest='new_mac', help='New MAC address')
+    options = parser.parse_args()
+    if not options.interface or not options.new_mac:
+        parser.error("[-] Please specify an interface and a new_mac address, use --help for more info.")
+    return options
 
-interface = args.interface
-new_mac = args.new_mac
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.run(["ifconfig", interface, "down"])
+    subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.run(["ifconfig", interface, "up"])
 
-subprocess.run(["ifconfig", interface, "down"])
-subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.run(["ifconfig", interface, "up"])
+
+args = get_arguments()
+change_mac(args.interface, args.new_mac)
 

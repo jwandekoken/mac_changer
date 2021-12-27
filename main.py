dfaa6fd82cd3f16d4094f05c50d92425430092ce
Julio Wandekoken <jwandekoken@gmail.com>
2021-12-27 15:10:10 -0500

    chore: implemented check to see if mac address was changed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import re
 
 
 def get_arguments():
@@ -19,6 +20,23 @@ def change_mac(interface, new_mac):
     subprocess.run(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
 args = get_arguments()
+current_mac = get_current_mac(args.interface)
+print("current_mac: ", current_mac)
+
 change_mac(args.interface, args.new_mac)
 
+current_mac = get_current_mac(args.interface)
+if current_mac == args.new_mac:
+    print("[+] MAC address was successfully changed to: ", current_mac)
+else:
+    print("[-] MAC address did not get changed.")
